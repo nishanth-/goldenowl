64043fb0fbcdb460504266b0dd0a2278e5be092e
nishanth- <nishanth.salian@gmail.com>
2021-08-22 20:35:45 +0530

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
-    name = 'goldenowl',         # How you named your package folder (MyLib)
+    name = 'goldowl',         # How you named your package folder (MyLib)
     packages = [
                 'goldenowl',
                 'goldenowl.portfolio'
                 ],   # Chose the same as "name"
-    version = '0.1',      # Start with a small number and increase it with every change you make
+    version = '0.4',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description = 'Tools for financial analysis',   # Give a short description about your library
     author = 'Nishanth (nishanth-)',                   # Type in your name
     author_email = '',      # Type in your E-Mail
     url = 'https://github.com/nishanth-/goldenowl',   # Provide either the link to your github or to your website
     download_url = 'https://github.com/nishanth-/goldenowl/archive/refs/tags/v_01.tar.gz',    # I explain this later on
     keywords = ['finance', 'investment', 'quant'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
-                      'pandas==1.1.5',
-                      'xirr==0.1.6',
+                      'pandas>=1.1.5',
+                      'xirr>=0.1.6',
                      ],
     classifiers=[
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
