c7e280e7a2bfca319ac3957a9801c22cd14c73fd
nishanth- <nishanth.salian@gmail.com>
2021-08-23 09:34:36 +0530

    clean up files

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 setup(
-    name = 'goldenowl',         # How you named your package folder (MyLib)
+    name = 'goldenowl',         # Name of pip install package 
     packages = [
                 'goldenowl',
                 'goldenowl.portfolio'
-                ],   # Chose the same as "name"
-    version = '2.0',      # Start with a small number and increase it with every change you make
+                ],   # All packages in the project
+    version = '2.0',      # Canonical versioning
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    author = 'Nishanth (nishanth-)',                   # Type in your name
-    author_email = '',      # Type in your E-Mail
-    url = 'https://github.com/nishanth-/goldenowl',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/nishanth-/goldenowl/archive/refs/tags/v_20.tar.gz',    # I explain this later on
-    keywords = ['finance', 'investment', 'quant'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
+    author = 'Nishanth (nishanth-)',
+    author_email = '',
+    url = 'https://github.com/nishanth-/goldenowl',   # Link to home page
+    download_url = 'https://github.com/nishanth-/goldenowl/archive/refs/tags/v_20.tar.gz',    # Link to release tar file
+    keywords = ['finance', 'investment', 'quant'],
+    install_requires=[            # All dependencies from pipreqs <folder>
                       'pandas>=1.1.5',
                       'xirr>=0.1.6',
                      ],
@@ -20,8 +20,8 @@ setup(
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',      # Define that your audience are developers
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: MIT License',   # License
+        'Programming Language :: Python :: 3',      #Supported versions
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
